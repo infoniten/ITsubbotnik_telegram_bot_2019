6e00929f2b6510bb13ce05bea8397dc55a2ac8d7
Valerii_Fomin <Valerii_Fomin@epam.com>
2019-09-23 16:17:58 +0300

    Add spring and token&name store in application.yml

--- src/main/java/com/epam/Main.java
+++ src/main/java/com/epam/Main.java
@@ -1,5 +1,27 @@
 package com.epam;
 
 
-public class Main {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+//        ApiContextInitializer.init();
+//
+//        TelegramBotsApi botsApi = new TelegramBotsApi();
+//
+//        try {
+//            botsApi.registerBot(new MyAmazingTestBot());
+//        } catch (TelegramApiException e) {
+//            e.printStackTrace();
+//        }
+    }
 }
--- src/main/java/com/epam/telegram/MyAmazingTestBot.java
+++ src/main/java/com/epam/telegram/MyAmazingTestBot.java
@@ -1,19 +1,43 @@
 package com.epam.telegram;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+@Component
 public class MyAmazingTestBot extends TelegramLongPollingBot {
+    @Value("${telegram.bot.username}")
+    private String botName;
+    @Value("${telegram.bot.token}")
+    private String botToken;
+
 
     public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
 
+            SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText(message_text);
+            try {
+                execute(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public String getBotUsername() {
-        return null;
+        return botName;
     }
 
     public String getBotToken() {
-        return null;
+        return botToken;
     }
 }
